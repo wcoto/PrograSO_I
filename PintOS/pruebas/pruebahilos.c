fa86998a17150f09618abe9ffabd5c3115842328
giobando <gilberth2005@hotmail.com>
2017-04-17 21:58:18 -0600

    agregando validaciones

--- PintOS/pruebas/pruebahilos.c
+++ PintOS/pruebas/pruebahilos.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[]) {
 	
@@ -9,9 +10,11 @@ int main (int argc, char *argv[]) {
 	// printf("\ncmdline args count=%d", argc);
 
 	int numThreads = 25; 		//maximo 25
-	bool typeThread = 0;		//0: I/O bound, 1: CPU Bound
+	bool typeThread = false;	//0: I/O bound, 1: CPU Bound
 	char archivo[30];			//nombre del archivo
 	int percent = 0;			//porcentaje del hilo I/O vs CPU.
+	bool using_b= false;		//indica si el argumento typethread se ingreso para no permitir el uso del otro.
+	bool using_p = false;
 
 	if(numArgs%2 == 0){			//en caso de algun parametro no tenga valores!
 		printf("Error! Los argumentos estan incompletos\n");
@@ -25,16 +28,23 @@ int main (int argc, char *argv[]) {
 			}
 			else if(!strcmp(argv[i],"-b")){
 				printf("Digito -b valor: %s \n",argv[i+1]);	
+				using_b = true;
 			}
 			else if(!strcmp(argv[i], "-l")){
 				printf("Digito -l valor: %s \n",argv[i+1]);
 			}else if(!strcmp(argv[i], "-p")){
 				printf("Digito -p valor: %s \n",argv[i+1]);
+				using_p = true;
 			}
-			printf("\narg%d=%s\n", i, argv[i]);
+			
 		}
+	}
+
+	if(using_p && using_b){
+		printf("Error, no esta permitido usar -b y -p simultaneamente\n");
+		exit(0);
 	}	
 
-	printf("\n");
+
 	return 0;
 }
